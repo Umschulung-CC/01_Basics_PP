7f3a3a2aa2966d52ddc0571ed6924d755440defc
Git4hobby <Markus_Clephas@t-online.de>
2025-01-03 09:51:57 +0000

    02 / Deklaration und Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,7 +1,43 @@
-console.log("Bauer sucht Frau Frohes Neues!");
+// Zeilenkommtar 
+// console.log("Bauer sucht Frau Frohes Neues!");
 
 /**Blockkommentar
  * Heute ist ein guter Tag zum Sterben
  * morgen läuft der Osterhase ohne Ostergras
  * 
  */
+/***** Variablen 01 *******/
+
+// console.log("Hi du Assi!");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Köperklaus" // Wertzuweisung | assignment
+// console.log(firstName); // Ausgabe#
+// let familyname = "Körperkasper"; // Deklaration + Wertzuweisung
+// console.log(" Hi, ich bin " + firstName + " " + familyname + "!");
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+let test; // Deklaration 
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------");
+test = "Hodenrodler"
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------");
+test = 2;
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------");
+test = true;
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------");
