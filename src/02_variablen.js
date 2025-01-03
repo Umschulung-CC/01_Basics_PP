36e5ae206e470a2b45c29d61fe83b5c933620d5d
Git4hobby <Markus_Clephas@t-online.de>
2025-01-03 10:09:11 +0000

    02 // Variable vs. Konstante 2

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -43,3 +43,13 @@
 // console.log("-------");
 
 /* Variablen vs. Konstanten in JS */
+//Variable
+let test;  // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi Hodenrodler!"
+test2 = "Hello!"; // Laufzeitfehler !!
+console.log("Inhalt: " + test2); // Ausgabe
