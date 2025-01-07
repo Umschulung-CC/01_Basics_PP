3801e1d425d66afbe53d93d43ce397098653b4a0
Git4hobby <Markus_Clephas@t-online.de>
2025-01-07 14:43:49 +0000

    06_controlflow04

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -76,50 +76,50 @@ const firstName = "Jane";
 let job;
 
 // job = "driver";  // .. fährt TAXI! / UBER
-// job = "diver"; // .. taucht im Rhein! 
+job = "diver"; // .. taucht im Rhein! 
 // job = "artist"; // .. malt ein Bild!
 // job = "pilot"; // .. macht etwas anderes! --> default
 // job = "teacher"; // .. unterrichtet!
 // job = "instructor"; // .. unterrichtet!
 
-// switch (job) 
-// {
-//     case "driver":  // job == "driver"
-//         console.log(firstName + " fährt TAXI / Uber!")
-//         break;
-//     case "diver":  // job == "diver"
-//         console.log(firstName + " taucht im Rhein!")
-//         break;
-//     case "artist":  // job == "artist"
-//         console.log(firstName + " malt ein Bild!")
-//         break;
-//     case "pilot":  // job == "pilot"
-//         console.log(firstName + " macht etwas anderes! --> default!")
-//         break;
-//     case "teacher":  // job == "teacher"
-//     case "instructor":  // job == "instructor"
-//         console.log(firstName + " unterrichtet!")
-//         break; 
-//     default: // WICHTIG
-//         console.log(firstName + " hier ist ein Fehler du Bauer!")
-//         break;
-// }
+switch (job) 
+{
+    case "driver":  // job == "driver"
+        console.log(firstName + " fährt TAXI / Uber!")
+        // break;
+    case "diver":  // job == "diver"
+        console.log(firstName + " taucht im Rhein!")
+        // break;
+    case "artist":  // job == "artist"
+        console.log(firstName + " malt ein Bild!")
+        // break;
+    case "pilot":  // job == "pilot"
+        console.log(firstName + " macht etwas anderes! --> default!")
+        // break;
+    case "teacher":  // job == "teacher"
+    case "instructor":  // job == "instructor"
+        console.log(firstName + " unterrichtet!")
+        // break; 
+    default: // WICHTIG
+        console.log(firstName + " hier ist ein Fehler du Bauer!")
+        break;
+}
 
 /********  Fallunterscheidung / SWITCH|CASE 2 *******/
 
-const cond = true;
-const value = 2;
+// const cond = true;
+// const value = 2;
 
-switch (cond) {
-    case (value == 1):
-        console.log("Value hat den Wert 1.");
-        break;
-    case (value == 2):
-        console.log("Value hat den Wert 2.");
-        break;
-    default:
-        console.log("Value hat einen anderen Wert.");
-        break;
-}
+// switch (cond) {
+//     case (value == 1):
+//         console.log("Value hat den Wert 1.");
+//         break;
+//     case (value == 2):
+//         console.log("Value hat den Wert 2.");
+//         break;
+//     default:
+//         console.log("Value hat einen anderen Wert.");
+//         break;
+// }
 
 
