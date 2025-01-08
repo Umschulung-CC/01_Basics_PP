e49a21dcb539b865236b0d1d2492e3d614641a91
Git4hobby <Markus_Clephas@t-online.de>
2025-01-08 08:09:22 +0000

    08_Kapselung von Codeblöcken

---
+++ src/08_Funktionen_I.js
@@ -0,0 +1,13 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+test();
+
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test ()
+{
+    console.log("Hallo, du Progammier Noob!");
+}
