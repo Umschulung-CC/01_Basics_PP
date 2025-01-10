7d3974e1e15e49f1734b5cd912e4ef306495e6a1
Git4hobby <Markus_Clephas@t-online.de>
2025-01-10 11:52:18 +0000

    11_Arrays_2. Variante: Struktur erzeugen + trimmen

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,48 +5,86 @@
  * 
  */
 
-
 /*** 02. Theorie: Array */
 
 // let arr;
-// // arr = new Array(); / Konstruktor
-// arr = [] // Literal
-// arr = [1,2,3,4,5,6,7,8];
+// // arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
 
 // output(arr);
-// output(arr.length); // Azahl der Elemente
-// output(arr [0]); // Index 0 (1. Pos. im Arry)
-// output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letze Pos. / allgemein
+
 
 /* 02a. Theorie: Schleifen (for-schleife) */
 /* For -Schleifen als allg. Wiederholungs-Struktur */
 
-// Inkrement (unter Grenze --> obere Grenze)
+// Inkrement (untere Grenze --> obere Grenze)
 // for (let i = 0; i < 10; i++) 
-//     {
-//         output("index: :" + i);
-//     }
-    
-    // Dekrement (obere Grenze --> untere Grenze)
-    // for (let i = 10; i > 0; i--) 
-    // {
-    //     output("index: " + i);
-    // }
+// {
+//     output("index: " + i);
+// }
 
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) 
+// {
+//     output("index: " + i);
+// }
+    
 // Inkrement (var. Schrittweite)
 // for (let i = 10; i <= 100; i += 5) 
-//     {
-//         output("index: " + i);   
-//     }
+// {
+//     output("index: " + i);   
+// }
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) 
-{
-   output(i); 
-   output(arr[i]);
-}
-   
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+//    output(i); 
+//    output(arr[i]);
+// }
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    a += 1; // a = a + 1
+//    output("inhalt von a: " + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    str += "Test" + " "; // a = a + 1
+//    output(str);
+// }
+
 
 
 
@@ -59,41 +97,73 @@ for (let i = 0; i < arr.length; i++)
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-// output(getSentence(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 word5 + GAP + 
+//                 word6 +
+//                 PUNCT;
 
-function getSentence(word1,word2,word3,word4,word5,word6) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                word5 + GAP +
-                word6 +  
-                PUNCT;
-
-    return str;
-}
+//     return str;
+// }
 
 /*** 01a. Funktionalität mit Array 1 */
 // Kritik | Überlegung
-// word1,word2,....wordN :: Semantische Struktur
-// arr[0],arr[1]... arr[n-1] :: Numerische Struktur (0,1,,3...n)
-// --> Transformation sematisches Problem --> numerisches Problem
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
 // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] +  
-                PUNCT;
+// function getSentenceArr(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   arr[0] + GAP +
+//                 arr[1] + GAP +
+//                 arr[2] + GAP +
+//                 arr[3] + GAP +
+//                 arr[4] + GAP + 
+//                 arr[5] +
+//                 PUNCT;
 
-    return str;
-}
+//     return str;
+// }
+
+// /*** 01b. Funktionalität mit Array 2  */
+// // Transponierung:  untereinander ---> nebeneinander
+// // Helge Schneider: Anananandereihung ...
+// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// function getSentenceArr2(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =  "";
+
+//     // 1. Variante: Struktur GENAU erzeugen ...
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i != arr.length - 1) { // wenn du noch nicht am Ende des Arrays bist ...
+//             str += arr[i] + GAP; // entweder ..
+//         } else {
+//             str += arr[i] + PUNCT;  // oder ...
+//         }
+//     }
+
+//     return str;
+
+
+// }
+
+// 2. Variante: Struktur erzeugen + trimmen()
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i] + GAP; // entweder ..
+    }
+    
+    return str.trim() + PUNCT;
+
+
+
 
 
 
